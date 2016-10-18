4bb102e43d848980643eaa6b6733ec892c683057
Rudieres Lima <rudiereslima@gmail.com>
2016-10-18 10:59:56 -0300

    Adicionando outros carros e textos

--- Street Race.py
+++ Street Race.py
@@ -12,6 +12,7 @@ y = 600
 
 relogio = pygame.time.Clock()
 
+
 tela = pygame.display.set_mode((x, y))
 pygame.display.set_caption ('Street Race')
 
@@ -25,6 +26,12 @@ fundo = pygame.image.load(diretorio)
 diretorio = os.path.join('imagens', 'Carro2.png')
 carro2 = pygame.image.load(diretorio)
 
+diretorio = os.path.join('imagens', 'Carro1.png')
+carro3 = pygame.image.load(diretorio)
+
+diretorio = os.path.join('imagens', 'Carro3.png')
+carro4 = pygame.image.load(diretorio)
+
 diretorio = os.path.join("imagens", "pessoa.png")
 pessoa1 = pygame.image.load(diretorio)
 
@@ -33,7 +40,7 @@ policial = pygame.image.load(diretorio)
 
 diretorio = os.path.join("fontes", "Aero Matics Bold.ttf")
 fonte = pygame.font.Font(diretorio, 24)
-
+fonte2 = pygame.font.Font(diretorio, 48)
 		
 ax = 0
 by = 0
@@ -45,7 +52,7 @@ def carros():
 	if key[pygame.K_DOWN]:
 		by = by + 2
 	else:
-		by = by + 5	
+		by = by + 6	
 	if criar:
 		ax = random.randint(214, 584)
 		by = 0
@@ -55,6 +62,49 @@ def carros():
 		criar = True
 			
 	tela.blit(carro2, (ax - iraio, by - iraio))
+
+ax3 = 350
+by3 = 0
+criar1 = True
+iraio3 = 25
+def carros2():
+	global criar1, ax3, by3, iraio3, by
+	key = pygame.key.get_pressed()
+	if key[pygame.K_DOWN]:
+		by3 = by3 + 4
+	else:
+		by3 = by3 + 8	
+
+	if by > 300 and criar1:
+		ax3 = random.randint(214, 584)
+		by3 = 0
+		criar1 = False
+
+	if by3 > 600:
+		criar1 = True
+			
+	tela.blit(carro3, (ax3 - iraio3, by3 - iraio3))
+ax4 = 280
+by4 = 0
+criar2 = True
+iraio4 = 27
+def carros3():
+	global criar2, ax4, by4, iraio4, by
+	key = pygame.key.get_pressed()
+	if key[pygame.K_DOWN]:
+		by4 = by4 + 6
+	else:
+		by4 = by4 + 10	
+
+	if by > 500 and criar2:
+		ax4 = random.randint(214, 584)
+		by4 = 0
+		criar2 = False
+
+	if by4 > 600:
+		criar2 = True
+			
+	tela.blit(carro4, (ax4 - iraio4, by4 - iraio4))
 	
 xa = 620
 yb = 0
@@ -69,6 +119,7 @@ def pessoa():
 	
 	xa = xa - 2
 	yb = yb + 2
+		
 	if travessia:
 		yb = random.randint(140, 290)
 		travessia = False
@@ -81,6 +132,9 @@ def pessoa():
 	if aa >= (98*2):
 		aa = 0
 	
+	if xa <= -200:
+		xa = 620
+		travessia = True
 		
 	tela.blit(pessoa1, (xa-praio, yb-praio), (aa, 0, larg, alt))  
 
@@ -119,6 +173,10 @@ def policia():
 		py = random.randint(100, 250)
 		multa = False
 	
+	if py >= 1200:
+		py = 100
+		multa = True
+	
 	tela.blit(policial, (px-mraio, py-mraio))  
 
 
@@ -150,22 +208,35 @@ def texto():
 jogando = True
 segundo = 120
 while jogando:
+	relogio.tick(60)
+
 	for evento in pygame.event.get():
 		if evento.type == pygame.QUIT:
 			jogando = False
 	
 	segundo = segundo - 0.02
 	
-	if segundo == 0:
+	if segundo <= 0:
+		surface = fonte2.render("You Win!", True, Branco)
+		tela.blit(surface, (300, 300))	
 		jogando = False
 		time.sleep(5)
 		pygame.display.quit()	
 
 	pista()
 	carros()
+	carros2()
 	player()	
 	texto()
-	policia()
+		
+	if segundo <= 85:
+		pessoa()
+
+	if segundo <= 65:
+		policia()
+
+	if segundo <= 50:
+		carros3()
 	
 	distancia = math.sqrt((ax-ax1)**2 + (by - by1)**2)
 	if distancia < (iraio+nraio):
@@ -179,9 +250,22 @@ while jogando:
 	distancia2 = math.sqrt((by1-py)**2)
 	if distancia2 < (iraio+mraio) and not key[pygame.K_DOWN]:
 		vida = 0
+	
+	distancia3 = math.sqrt((ax1-ax3)**2 + (by1 - by3)**2)
+	if distancia3 < (iraio3+nraio):
+		vida = 0
+	
+	distancia4 = math.sqrt((ax1-ax4)**2 + (by1 - by4)**2)
+	if distancia4 < (iraio4+nraio):
+		vida = 0
 
+	if vida <= 0:
+		surface = fonte2.render("Game Over!", True, Branco)
+		tela.blit(surface, (300, 300))	
+		jogando = False
+	
 	pygame.display.update()
-
+	
 	if vida < 1: 
 		time.sleep(5)	
 		pygame.display.quit()
